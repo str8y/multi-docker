8da7c4d5c17dbdbcc84891dbec14ceed325aeb80
Daniel Feit <danielfeit@Daniels-iMac.fritz.box>
2020-10-20 09:00:54 +0200

    initial commit

---
+++ client/src/Fib.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default () => {
+  const [seenIndexes, setSeenIndexes] = useState([]);
+  const [values, setValues] = useState({});
+  const [index, setIndex] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      const values = await axios.get('/api/values/current');
+      setValues(values.data);
+
+      const seenIndexes = await axios.get('/api/values/all');
+      setSeenIndexes(seenIndexes.data);
+    }
+    fetchData();
+  }, []);
+
+  const renderValues = () => {
+    const entries = [];
+
+    for (const key in values) {
+      entries.push(
+        <div key={key}>
+          For index
+          {' '}
+          {key}
+          {' '}
+          I calculated
+          {' '}
+          {values[key]}
+        </div>,
+      );
+    }
+    return entries;
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await axios.post('/api/values', {
+      index,
+    });
+    setIndex('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Enter your index:</label>
+        <input
+          value={index}
+          onChange={(event) => setIndex(event.target.value)}
+        />
+        <button>Submit</button>
+      </form>
+
+      <h3>Indexes I have seen:</h3>
+      {seenIndexes.map(({ number }) => number).join(', ')}
+      <h3>Calculated Values:</h3>
+      {renderValues()}
+    </div>
+  );
+};
